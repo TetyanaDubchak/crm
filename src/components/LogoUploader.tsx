5d80d2ef00f19d4152fbb9fb3b716b284c2c41b5
TetyanaDubchak <123674629+TetyanaDubchak@users.noreply.github.com>
2025-03-08 20:30:42 +0200

    Add upload Logo

--- src/components/LogoUploader.tsx
+++ src/components/LogoUploader.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import Image from 'next/image';
 import Icon from './Icon';
 import s from '@/styles/components/LogoUploader.module.scss';
 
@@ -12,13 +13,51 @@ export default function LogoUploader({
   label,
   placeholder,
 }: LogoUploaderProps) {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [image, setImage] = useState<string | null>(null);
+
+  const handleClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setImage(imageUrl);
+    }
+  };
+
   return (
     <div className={s.wrapper}>
       {label && <p className={s.title}>{label}</p>}
-      <label className={s['choose-wrapper']} htmlFor="logo">
-        <Icon type="cloud" />
-        {placeholder && <p>{placeholder}</p>}
-        <input type="file" accept="image/*" className={s['choose-file']} />
+      <label
+        className={s['choose-wrapper']}
+        onClick={handleClick}
+        htmlFor="logo"
+      >
+        {image ? (
+          <Image
+            src={image}
+            alt="Uploaded photo"
+            width={176}
+            height={176}
+            className={s['picture']}
+          />
+        ) : (
+          <>
+            <Icon type="cloud" />
+            {placeholder && <p>{placeholder}</p>}
+          </>
+        )}
+
+        <input
+          ref={fileInputRef}
+          type="file"
+          accept="image/*"
+          className={s['choose-file']}
+          onChange={handleFileChange}
+        />
       </label>
     </div>
   );
